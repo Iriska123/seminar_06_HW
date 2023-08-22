acf769da8e596a75493b949d82284c58c351c274
Irina <irinapetr1110@mail.ru>
2023-08-22 18:15:14 +0300

    task 1_1

--- task_01/Program.cs
+++ task_01/Program.cs
@@ -6,26 +6,21 @@
 1, -7, 567, 89, 223-> 4*/
 
 
-int M = 5;
-Console.WriteLine($"Введите {M} чисел через пробел: ");
+
+Console.WriteLine($"Введите несколько чисел через пробел: ");
 string enter = Console.ReadLine();
+Console.WriteLine(enter);
 string[] array = enter.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-
-
-Console.WriteLine(String.Join(",",array));
-
-int result = Console.WriteLine(CountNums(array)!);
+Console.WriteLine(String.Join(",", array));
+int result = Console.WriteLine(CountNums(array));
 
 int CountNums(string[] array){
     int count = 0;
     foreach (string item in array)
     {
-        //int v = Convert.ToInt32(item.ToString());
-        if(Convert.ToInt32((item.ToString())) > 0) {
-            count++;
-        }
+        int v = Convert.ToInt32(item.ToString());
+        if(v > 0) count++;
     }
-    Console.WriteLine(count);
 return count;
 }
 
