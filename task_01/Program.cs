cecbb5234987d0b32ab95f6d7070fc87ecaff225
Irina <irinapetr1110@mail.ru>
2023-08-22 14:55:30 +0300

    tast 01 in process

--- task_01/Program.cs
+++ task_01/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл 
+пользователь. (Можно изучить метод Split.
+Пример: stringArray.Split(' ', StringSplitOptions.RemoveEmptyEntries);, где первый аргумент
+ это разделитель чисел, второе обработчик введеной последовательности)
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 4*/
+
+
+int M = 5;
+Console.WriteLine($"Введите {M} чисел через пробел: ");
+string enter = Console.ReadLine();
+string[] array = enter.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+
+Console.WriteLine(String.Join(",",array));
+
+int result = Console.WriteLine(CountNums(array)!);
+
+int CountNums(string[] array){
+    int count = 0;
+    foreach (string item in array)
+    {
+        //int v = Convert.ToInt32(item.ToString());
+        if(Convert.ToInt32((item.ToString())) > 0) {
+            count++;
+        }
+    }
+    Console.WriteLine(count);
+return count;
+}
+
